20952e85bc90baaa8e55b486988f153da2eb1a03
Zachary Patten <zpatten@jovelabs.com>
2013-03-30 22:28:07 -0700

    refactor roles

--- roles/base.rb
+++ roles/base.rb
@@ -1,5 +1,18 @@
 name "base"
 description "sample base role"
 run_list(
-  "recipe[motd-tail]"
+  "recipe[motd-tail]",
+  "recipe[timezone]"
+  "recipe[users]",
+  "recipe[sudo]"
+)
+
+default_attributes(
+  "authorization" => {
+    "groups" => ["devops"],
+    "sudo" => {
+      "users" => {"deployer" => true},
+      "groups" => {"devops" => false}
+    }
+  }
 )
