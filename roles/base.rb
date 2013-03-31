c448063e1302e665278cb89afbbb395cd18fab0e
Zachary Patten <zpatten@jovelabs.com>
2013-03-31 13:38:07 -0700

    add bluepill; turn on passwordless sudo

--- roles/base.rb
+++ roles/base.rb
@@ -1,18 +1,19 @@
 name "base"
 description "sample base role"
 run_list(
+  "recipe[bluepill]",
   "recipe[motd-tail]",
   "recipe[timezone]",
   "recipe[users]",
   "recipe[sudo]"
 )
 
-default_attributes(
+override_attributes(
   "authorization" => {
     "groups" => ["devops"],
     "sudo" => {
-      "users" => {"deployer" => true},
-      "groups" => {"devops" => false}
+      "users" => { "deployer" => true },
+      "groups" => { "devops" => true }
     }
   }
 )
