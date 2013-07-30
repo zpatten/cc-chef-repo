75c3422c22efb98b3d402e2d20cd1f15c3f6c8fe
pikesley <github@orgraphone.org>
2013-07-30 13:32:10 +0100

    Add more of our stuff

--- roles/base.rb
+++ roles/base.rb
@@ -1,18 +1,3 @@
 name "base"
 description "sample base role"
-run_list(
-  "recipe[motd-tail]",
-  "recipe[timezone]",
-  "recipe[users]",
-  "recipe[sudo]"
-)
 
-override_attributes(
-  "authorization" => {
-    "groups" => ["devops"],
-    "sudo" => {
-      "users" => { "deployer" => true },
-      "groups" => { "devops" => true }
-    }
-  }
-)
