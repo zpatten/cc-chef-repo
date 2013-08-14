f90c92f41dd0d32a28fcc7600ac1deced5850819
pikesley <github@orgraphone.org>
2013-08-14 11:40:20 +0100

    Oops, needs license stuff

--- site-cookbooks/quirkafleeg-deployment/metadata.rb
+++ site-cookbooks/quirkafleeg-deployment/metadata.rb
@@ -1,9 +1,9 @@
 name             'quirkafleeg-deployment'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures quirkafleeg-deployment'
+maintainer       'The Open Data Institute'
+maintainer_email 'tech@theodi.org'
+license          'MIT'
+description      'Manages deployments for quirkafleeg components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends "nginx"
