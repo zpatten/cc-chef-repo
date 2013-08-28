043d1d0131f1329c3aaec2400b2bd4cf59bc9c99
pikesley <github@orgraphone.org>
2013-08-28 17:35:17 +0100

    tidy up a bunch of stuff

--- roles/juvia-webnode.rb
+++ roles/juvia-webnode.rb
@@ -1,27 +1,26 @@
-name "juvia"
+name "juvia-webnode"
 
 default_attributes(
-    :users              => [
-        'juvia',
-        'quirkafleeg'
-    ],
-    'envbuilder'  => {
-        'base_dir' => '/home/quirkafleeg',
-        'owner' => 'quirkafleeg',
-        'group' => 'quirkafleeg'
-    },
+    :user              => 'juvia',
     :rvm               => {
         :user => 'juvia',
         :ruby => '1.9.3'
     },
     :project_fqdn      => 'juvia.theodi.org',
-    :mysql_db          => 'juvia',
-    :memcached_node    => 'juvia',
     :git_project       => 'juvia',
     :migration_command => 'bundle exec rake db:migrate',
 )
 
-run_list "role[base]",
-         "recipe[odi-rvm]",
-         "recipe[envbuilder]",
-         "recipe[odi-deployment]"
+override_attributes(
+    :deployment => {
+        :precompile_assets => true
+    }
+)
+
+run_list(
+    "role[base]",
+    "recipe[odi-rvm]",
+    "recipe[envbuilder]",
+    "recipe[odi-nginx]",
+    "recipe[odi-deployment]"
+)
