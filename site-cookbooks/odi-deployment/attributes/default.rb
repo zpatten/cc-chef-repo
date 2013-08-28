3a99b1b65d7affe7de55ef2623a92127a444fccc
pikesley <github@orgraphone.org>
2013-08-28 17:37:38 +0100

    Tidy this stuff up

--- site-cookbooks/odi-deployment/attributes/default.rb
+++ site-cookbooks/odi-deployment/attributes/default.rb
@@ -1,4 +1,5 @@
-default['thin_concurrency'] = 1
-default['env_path'] = "/home/env/env"
-default['deploy']['revision'] = "CURRENT"
-default['deploy']['migrate'] = true
+default[:deployment][:root]     = "/var/www"
+default[:deployment][:rack_env] = "production"
+default[:deployment][:revision] = "CURRENT"
+default[:deployment][:port]     = 3000
+default[:deployment][:domain]   = "theodi.org"
