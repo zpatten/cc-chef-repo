2f2e02535b23f2941f42e15e3ba71071f93a44b3
pikesley <github@orgraphone.org>
2013-09-02 09:57:57 +0100

    Bumpety bump

--- site-cookbooks/odi-deployment/metadata.rb
+++ site-cookbooks/odi-deployment/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'tech@theodi.org'
 license          'MIT'
 description      'New Improved Deployment Mechanism'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.3.2'
 
 depends "nginx"
