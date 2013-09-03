9fee9b605acc39bfbad7588f0b4e5a3e553c60cf
pikesley <github@orgraphone.org>
2013-09-03 17:47:39 +0100

    Bake the cron stuff into this, innit

--- roles/chef-client.rb
+++ roles/chef-client.rb
@@ -1,18 +1,30 @@
 name "chef-client"
 description "sample chef-client role"
 run_list(
-  "recipe[chef-client::delete_validation]",
-  "recipe[chef-client::config]",
-  "recipe[chef-client::cron]"
+    "recipe[chef-client::delete_validation]",
+    "recipe[chef-client::config]",
+    "recipe[chef-client::cron]"
+)
+
+default_attributes(
+    'chef_client' => {
+        'cron'  => {
+            'use_cron_d' => true,
+            'hour'       => "*",
+            'minute'     => "*/5",
+            'log_file'   => "/var/log/chef/cron.log"
+        },
+        'splay' => 250
+    }
 )
 
 override_attributes(
-  :chef_client => {
-    :server_url => "https://192.168.33.10",
-    :interval => 300,
-    :splay => 300,
-    :backup_path => "/var/chef/backup",
-    :cache_path => "/var/chef/cache",
-    :checksum_cache_path => "/var/chef/cache/checksums"
-  }
+    :chef_client => {
+        :server_url          => "https://192.168.33.10",
+        :interval            => 300,
+        :splay               => 300,
+        :backup_path         => "/var/chef/backup",
+        :cache_path          => "/var/chef/cache",
+        :checksum_cache_path => "/var/chef/cache/checksums"
+    }
 )
