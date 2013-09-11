4718c4e1e305a74f84ad319e2a272148159f3959
pikesley <github@orgraphone.org>
2013-09-11 15:49:46 +0100

    New thing

---
+++ roles/quirkafleeg-webnode.rb
@@ -0,0 +1,24 @@
+name "quirkafleeg-webnode"
+
+default_attributes(
+    'user' => 'quirkafleeg',
+    'rvm'  => {
+        'user' => 'quirkafleeg',
+        'ruby' => '1.9.3'
+    },
+    'envbuilder'  => {
+        'base_dir' => '/home/quirkafleeg',
+        'owner' => 'quirkafleeg',
+        'group' => 'quirkafleeg'
+    }
+)
+
+run_list(
+    "recipe[odi-rvm]",
+    "recipe[varnish]",
+    "recipe[envbuilder]",
+    "recipe[odi-nginx]",
+    # it would be loads better if this wasn't required
+    "recipe[capybara-webkit]",
+    "recipe[quirkafleeg-deployment]"
+)
