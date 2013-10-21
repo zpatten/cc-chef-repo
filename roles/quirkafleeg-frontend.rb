3454f4d7d75d5f71e24bf3186bed70581b599d27
pikesley <github@orgraphone.org>
2013-10-21 20:50:55 +0100

    Deprecate all these
    at least for now

--- roles/quirkafleeg-frontend.rb
+++ roles/quirkafleeg-frontend.rb
@@ -3,21 +3,18 @@ description "quirkafleeg frontend wrapper role"
 
 default_attributes(
     :apps => {
-        'people'           => {
-            :port => 3000
         },
-        'frontend-news'    => {
-            :deploy_name => 'news',
-            :port        => 3010
-        },
-        'frontend-www'     => {
-            :deploy_name => 'www',
-            :port        => 3020
-        },
-        'frontend-courses' => {
-            :deploy_name => 'courses',
-            :port        => 3030
-        },
+        #        'people'           => {
+        #            :port    => 3000
+        #        },
+        #        'frontend-news'    => {
+        #            :deploy_name => 'news',
+        #            :port        => 3010
+        #        },
+        #        'frontend-courses' => {
+        #            :deploy_name => 'courses',
+        #            :port        => 3030
+        #        },
     }
 )
 
