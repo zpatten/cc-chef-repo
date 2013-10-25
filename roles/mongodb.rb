75402e3463e6327367cba8174bc875e28ca700de
pikesley <github@orgraphone.org>
2013-10-25 13:49:19 +0100

    Hoppler

--- roles/mongodb.rb
+++ roles/mongodb.rb
@@ -1,7 +1,21 @@
 name "mongodb"
 description "mongodb server role"
 
+default_attributes(
+    'user' => 'hoppler'
+)
+
+override_attributes(
+    "envbuilder" => {
+        "base_dir" => "/home/hoppler/",
+        "filename" => ".env",
+        "owner"    => "hoppler",
+        "group"    => "hoppler"
+    }
+)
+
 run_list(
     "recipe[mongodb::10gen_repo]",
-    "recipe[odi-mongodb]"
+    "recipe[odi-mongodb]",
+    "recipe[hoppler]"
 )
