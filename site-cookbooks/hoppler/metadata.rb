c3ee813c386fc49f8fae151297a4a7e703852651
pikesley <github@orgraphone.org>
2013-11-01 15:28:45 +0000

    Bumpy bumpy bump bump

--- site-cookbooks/hoppler/metadata.rb
+++ site-cookbooks/hoppler/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'tech@theodi.org'
 license          'MIT'
 description      'Installs/Configures hoppler'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.1'
+version          '0.2.2'
 
 depends 'odi-rvm'
 depends 'envbuilder'
