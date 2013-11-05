60688a824d33e8bec1a5d7848203e5f6b76194bd
pikesley <github@orgraphone.org>
2013-11-05 10:41:22 +0000

    Tech Debt Excised

--- roles/mongodb.rb
+++ roles/mongodb.rb
@@ -2,17 +2,7 @@ name "mongodb"
 description "mongodb server role"
 
 default_attributes(
-    # THESE TWO ARE REQUIRED TO MAKE HOPPLER COOKBOOK WORK. THIS IS TECHNICAL DEBT. I'VE ALSO HAD TO ADD A
-    # MONGO_HOST ENV VAR. THIS IS VERY POOR. RECYCLING HOPPLER WAS A MISTAKE, I WILL FORK IT INSTEAD.
-    :databags     => {
-        :primary => 'quirkafleeg'
-    },
-    :database     => 'signon',
-    'user'    => 'hoppler',
-    'hoppler' => {
-        'backup_command' => 'backup_mongo',
-        'do_restore'     => false
-    }
+    'user'    => 'hoppler'
 )
 
 override_attributes(
@@ -28,5 +18,5 @@ run_list(
     "recipe[mysql::client]",
     "recipe[mongodb::10gen_repo]",
     "recipe[odi-mongodb]",
-    "recipe[hoppler]"
+    "recipe[hoppler::mongo]"
 )
