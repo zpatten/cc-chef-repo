b01d0d95ccb25625ecd632b8c20f45a21fac3d7c
pikesley <github@orgraphone.org>
2013-11-08 10:52:25 +0000

    'lastic

---
+++ site-cookbooks/odi-elasticsearch/metadata.rb
@@ -0,0 +1,9 @@
+name             'odi-elasticsearch'
+maintainer       'The Open Data Institute'
+maintainer_email 'tech@theodi.org'
+license          'MIT'
+description      'Installs/Configures odi-elasticsearch'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'elasticsearch'
