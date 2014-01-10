b094ac6d1be2812701debdb915dcaec30367f00d
pikesley <github@orgraphone.org>
2014-01-10 10:50:10 +0000

    bump

--- site-cookbooks/quirkafleeg-deployment/metadata.rb
+++ site-cookbooks/quirkafleeg-deployment/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'tech@theodi.org'
 license          'MIT'
 description      'Manages deployments for quirkafleeg components'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.20'
+version          '0.2.21'
 
 depends "nginx"
