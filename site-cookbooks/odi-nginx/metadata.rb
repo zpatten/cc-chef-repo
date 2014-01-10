c4b5b74c7e99c3b7162a4126f1f96bb6a6f52e64
pikesley <github@orgraphone.org>
2014-01-10 10:49:06 +0000

    Bump

--- site-cookbooks/odi-nginx/metadata.rb
+++ site-cookbooks/odi-nginx/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'tech@theodi.org'
 license          'MIT'
 description      'Installs/Configures odi-nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.3.0'
+version          '0.4.0'
