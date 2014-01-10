b080020b0a0fb75048fec56f172520fedffb4407
pikesley <github@orgraphone.org>
2014-01-10 10:44:08 +0000

    metrics

--- roles/metrics-api.rb
+++ roles/metrics-api.rb
@@ -2,25 +2,51 @@ name "metrics-api"
 description "metrics-api base role"
 
 default_attributes(
-    'chef_client' => {
-        'cron'  => {
-            'use_cron_d' => true,
-            'hour'       => "*",
-            'minute'     => "*/5",
-            'log_file'   => "/var/log/chef/cron.log"
+    :user              => 'metrics',
+    :rvm               => {
+        :default_ruby => '1.9.3'
+    },
+    :project_fqdn      => 'metrics.theodi.org',
+    :git_project       => 'metrics-api',
+    :chef_client       => {
+        :cron  => {
+            :use_cron_d => true,
+            :hour       => "*",
+            :minute     => "*/5",
+            :log_file   => "/var/log/chef/cron.log"
         },
-        'splay' => 250
+        :splay => 250
+    },
+    :deploy            => {
+        :revision => 'master'
+    },
+    :migration_command => 'bundle exec rake db:migrate',
+    :RACK_ENV          => 'production'
+)
+
+override_attributes(
+    "envbuilder" => {
+        "owner" => "env",
+        "group" => "env"
     }
 )
 
 run_list(
-    "recipe[odi-apt]",
-    "recipe[build-essential]",
-    "recipe[git]",
-    "role[chef-client]",
-    "recipe[postfix]",
-    "recipe[ntp]",
-    "recipe[odi-users]",
-    "recipe[odi-pk]",
-    "recipe[fail2ban]"
+    'role[chef-client]',
+    'recipe[odi-apt]',
+    'recipe[build-essential]',
+    'recipe[postfix]',
+    'recipe[git]',
+    'recipe[libcurl]',
+    'recipe[ntp]',
+    'recipe[odi-users]',
+    'recipe[odi-pk]',
+    'recipe[rvm::system]',
+    'recipe[envbuilder]',
+    'recipe[mongodb::10gen_repo]',
+    'recipe[odi-mongodb]',
+    'recipe[mysql::client]',
+    'recipe[hoppler::mongo]',
+    'recipe[odi-nginx]',
+    'recipe[odi-simple-deployment]'
 )
